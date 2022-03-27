078fc2573c9c10c00c8a43c05fb424fab390e522
iammrtndev <68447700+iammrtndev@users.noreply.github.com>
2022-03-27 20:47:47 +0200

    feat: transparent background

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const furiganaFont = "20px Noto Sans JP";
 const app = require("express")();
 app.use(require("cors")());
 
-app.get("/:text", function(req, res) {
-  renderFurigana(req.params.text, kanjiFont, furiganaFont).then(pngBuffer => {
+app.get("/:text", async (req, res) => {
+  try {
+    const png = await renderFurigana(req.params.text, kanjiFont, furiganaFont, {
+      backgroundColor: "transparent",
+      textColor: "white"
+    });
     res.contentType("image/jpeg");
-    res.send(pngBuffer);
-  });
+    res.send(png);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 app.listen(process.env.PORT || 3000);
